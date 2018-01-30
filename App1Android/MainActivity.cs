0c05207d9dc8ccfd03190c44151efb07263bebec
Martin Fetzel <martin.fetzel@get-the-solution.net>
2018-01-30 19:11:58 +0100

    Add project files.

---
+++ App1Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace App1Android
+{
+    [Activity(Label = "App1Android", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
